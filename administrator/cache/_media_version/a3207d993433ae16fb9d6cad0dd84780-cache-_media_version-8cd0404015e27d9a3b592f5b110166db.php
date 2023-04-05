f3abea9e53583fdeef23ca48b6f730020ee190d0
satananta <122503995+satananta@users.noreply.github.com>
2023-04-05 23:11:25 +0700

    Initial commit

---
+++ administrator/cache/_media_version/a3207d993433ae16fb9d6cad0dd84780-cache-_media_version-8cd0404015e27d9a3b592f5b110166db.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"1f54d50bc0cd0b2ac68c4ef8621e05fb";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-03-27 02:58:40 UTC
+#Software: Joomla! 4.2.8 Stable [ Uaminifu ] 16-February-2023 15:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-03-27T02:58:40+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-27T03:02:20+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
